0d2dee5e994d422e781ad194a3a412efcdbd0647
Nico Kuchling <nico.kuchling@outlook.de>
2024-03-09 14:32:58 +0100

    Iteration-composite-service initial commit

---
+++ src/main/java/com/nicokuchling/wegfest/iteration_composite_service/IterationCompositeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nicokuchling.wegfest.iteration_composite_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IterationCompositeServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IterationCompositeServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nicokuchling/wegfest/iteration_composite_service/IterationCompositeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicokuchling.wegfest.iteration_composite_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IterationCompositeServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
